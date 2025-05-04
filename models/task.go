2ae95ecec6327e79f4e1ae4db9e859ce172954f6
Muhammad Bintang Nugraha <djakarta1973@gmail.com>
2025-05-05 02:04:25 +0700

    add test migration

--- models/task.go
+++ models/task.go
@@ -3,13 +3,13 @@ package models
 import "time"
 
 type Task struct {
-	ID           int       `gorm:"type:int;primaryKey;autoIncrement" json:"id"`
-	UserID       int       `gorm:"type:int" json:"userId"`
+	Id           int       `gorm:"type:int; primaryKey; autoIncrement" json:"id"`
+	UserId       int       `gorm:"int" json:"userId"`
 	Title        string    `gorm:"type:varchar(255)" json:"title"`
 	Description  string    `gorm:"type:text" json:"description"`
 	Status       string    `gorm:"type:varchar(50)" json:"status"`
-	Reason       string    `gorm:"type:text;default:''" json:"reason"`
-	Revision     int       `gorm:"type:int;default:0" json:"revision"`
+	Reason       string    `gorm:"type:text; default:" json:"reason"`
+	Revision     int8      `gorm:"type:int; default:0" json:"revision"`
 	DueDate      string    `gorm:"type:varchar(50)" json:"dueDate"`
 	SubmitDate   string    `gorm:"type:varchar(50)" json:"submitDate"`
 	RejectedDate string    `gorm:"type:varchar(50)" json:"rejectedDate"`
@@ -17,6 +17,5 @@ type Task struct {
 	Attachment   string    `gorm:"type:varchar(255)" json:"attachment"`
 	CreatedAt    time.Time `json:"createdAt"`
 	UpdatedAt    time.Time `json:"updatedAt"`
-
-	User User `gorm:"foreignKey:UserID" json:"user,omitempty"` // belongs to
+	User         User      `gorm:"foreignKey:UserId" json:"user,omitempty"` // belongs to
 }
--- models/user.go
+++ models/user.go
@@ -8,16 +8,17 @@ import (
 )
 
 type User struct {
-	ID        int       `gorm:"type:int;primaryKey;autoIncrement" json:"id"`
+	Id        int       `gorm:"type:int;primaryKey;autoIncrement" json:"id"`
 	Role      string    `gorm:"type:varchar(10)" json:"role"`
 	Name      string    `gorm:"type:varchar(255)" json:"name"`
 	Email     string    `gorm:"type:varchar(50)" json:"email"`
 	Password  string    `gorm:"type:varchar(255)" json:"password"`
-	Tasks     []Task    `gorm:"constraint:OnDelete:CASCADE;" json:"tasks,omitempty"`
 	CreatedAt time.Time `json:"createdAt"`
 	UpdatedAt time.Time `json:"updatedAt"`
+	Tasks     []Task    `gorm:"constraint:OnDelete:CASCADE" json:"tasks,omitempty"` // has many
 }
 
-func (u User) AfterDelete(tx *gorm.DB) (err error) {
-	return tx.Clauses(clause.Returning{}).Where("user_id = ?", u.ID).Delete(&Task{}).Error
+func (u *User) AfterDelete(tx *gorm.DB) (err error) {
+	tx.Clauses(clause.Returning{}).Where("user_id = ?", u.Id).Delete(&Task{})
+	return
 }
