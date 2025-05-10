8bb86c412a2d8f7231c804d878a320f2523a581b
Muhammad Bintang Nugraha <djakarta1973@gmail.com>
2025-05-10 23:02:46 +0700

    setup main

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,125 +1,126 @@
+import 'package:frontend/common/app_color.dart';
+import 'package:frontend/common/app_route.dart';
+import 'package:frontend/data/models/user.dart';
+import 'package:frontend/presentation/bloc/detail_task/detail_task_cubit.dart';
+import 'package:frontend/presentation/bloc/employee/employee_bloc.dart';
+import 'package:frontend/presentation/bloc/list_task/list_task_bloc.dart';
+import 'package:frontend/presentation/bloc/need_review/need_review_bloc.dart';
+import 'package:frontend/presentation/bloc/progress_task/progress_task_bloc.dart';
+import 'package:frontend/presentation/bloc/stat_employee/stat_employee_cubit.dart';
+import 'package:frontend/presentation/bloc/user/user_cubit.dart';
+import 'package:frontend/presentation/pages/add_employee_page.dart';
+import 'package:frontend/presentation/pages/add_task_page.dart';
+import 'package:frontend/presentation/pages/detail_task_page.dart';
+import 'package:frontend/presentation/pages/home_admin_page.dart';
+import 'package:frontend/presentation/pages/home_employee_page.dart';
+import 'package:frontend/presentation/pages/list_task_page.dart';
+import 'package:frontend/presentation/pages/login_page.dart';
+import 'package:frontend/presentation/pages/monitor_employee_page.dart';
+import 'package:frontend/presentation/pages/profile_page.dart';
+
+import 'package:d_session/d_session.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import 'presentation/bloc/login/login_cubit.dart';
 
 void main() {
-  runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]).then((value) {
+    runApp(const MainApp());
+  });
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (context) => UserCubit()),
+        BlocProvider(create: (context) => LoginCubit()),
+        BlocProvider(create: (context) => NeedReviewBloc()),
+        BlocProvider(create: (context) => EmployeeBloc()),
+        BlocProvider(create: (context) => StatEmployeeCubit()),
+        BlocProvider(create: (context) => ProgressTaskBloc()),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData.light(useMaterial3: true).copyWith(
+          primaryColor: AppColor.primary,
+          colorScheme: ColorScheme.light(
+            primary: AppColor.primary,
+            secondary: AppColor.secondary,
+          ),
+          scaffoldBackgroundColor: AppColor.scaffold,
+          textTheme: GoogleFonts.poppinsTextTheme(),
+          appBarTheme: AppBarTheme(
+            surfaceTintColor: AppColor.primary,
+            backgroundColor: AppColor.primary,
+            foregroundColor: Colors.white,
+            titleTextStyle: const TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 18,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+          ),
+          popupMenuTheme: const PopupMenuThemeData(
+            color: Colors.white,
+            surfaceTintColor: Colors.white,
+          ),
+          dialogTheme: const DialogTheme(
+            surfaceTintColor: Colors.white,
+            backgroundColor: Colors.white,
+          ),
         ),
+        initialRoute: AppRoute.home,
+        routes: {
+          AppRoute.home: (context) {
+            return FutureBuilder(
+              future: DSession.getUser(),
+              builder: (context, snapshot) {
+                if (snapshot.data == null) return const LoginPage();
+                User user = User.fromJson(Map.from(snapshot.data!));
+                context.read<UserCubit>().update(user);
+                if (user.role == "Admin") return const HomeAdminPage();
+                return const HomeEmployePage();
+              },
+            );
+          },
+          AppRoute.addEmployee: (context) => const AddEmployeePage(),
+          AppRoute.addTask: (context) {
+            User employee = ModalRoute.of(context)!.settings.arguments as User;
+            return AddTaskPage(employee: employee);
+          },
+          AppRoute.detailTask: (context) {
+            int id = ModalRoute.of(context)!.settings.arguments as int;
+            return BlocProvider(
+              create: (context) => DetailTaskCubit(),
+              child: DetailTaskPage(id: id),
+            );
+          },
+          AppRoute.listTask: (context) {
+            Map data = ModalRoute.of(context)!.settings.arguments as Map;
+            return BlocProvider(
+              create: (context) => ListTaskBloc(),
+              child: ListTaskPage(
+                status: data['status'],
+                employee: data['employee'],
+              ),
+            );
+          },
+          AppRoute.login: (context) => const LoginPage(),
+          AppRoute.monitorEmployee: (context) {
+            User employee = ModalRoute.of(context)!.settings.arguments as User;
+            return MonitorEmployeePage(employee: employee);
+          },
+          AppRoute.profile: (context) => const ProfilePage(),
+        },
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
