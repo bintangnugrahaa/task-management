e18cb4ff42a7fb2b6dc9ba53c2e80108eba9ea57
Muhammad Bintang Nugraha <djakarta1973@gmail.com>
2025-05-10 23:05:36 +0700

    add create account source

--- frontend/lib/common/urls.dart
+++ frontend/lib/common/urls.dart
@@ -1,4 +1,4 @@
 class URLs {
-  static const host = 'http://192.168.43.221:8080';
+  static const host = 'http://192.168.80.1:8080';
   static String image(String fileName) => '$host/attachments/$fileName';
 }
